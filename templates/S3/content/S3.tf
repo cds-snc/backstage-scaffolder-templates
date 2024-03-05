88062cb8a9522cca22f244eed49d17fd91807ecf
Sylvia McLaughlin <sylvia.mclaughlin@cds-snc.ca>
2024-03-05 14:55:52 -0800

    Making only the bucket name and billing code value mandatory

--- templates/S3/content/S3.tf
+++ templates/S3/content/S3.tf
@@ -1,22 +1,6 @@
   module "S3" {
     source = "github.com/cds-snc/terraform-modules//S3?ref=v9.2.4"
-  
-    billing_tag_key = var.billing_tag_key
-    billing_tag_value  = var.billing_code
+ 
     bucket_name = var.bucket_name
-    block_public_acls = var.block_public_acls
-    block_public_policy = var.block_public_policy
-    bucket_prefix = var.bucket_prefix
-    critical_tag_key = var.critical_tag_key
-    critical_tag_value = var.critical_tag_value
-    force_destroy = var.force_destroy
-    ignore_public_acls = var.ignore_public_acls
-    kms_key_arn = var.kms_key_arn
-    lifecycle_rule = var.lifecycle_rule
-    logging = var.logging
-    object_lock_configuration = var.object_lock_configuration
-    replication_configuration = var.replication_configuration
-    restrict_public_buckets = var.restrict_public_buckets
-    tags = var.tags
-    versioning = var.versioning
+    billing_tag_value  = var.billing_tag_value
   }
