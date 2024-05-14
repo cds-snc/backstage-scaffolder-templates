c63d8b548eebfcde027d9d6aeab0193c8ca02344
Sylvia McLaughlin <sylvia.mclaughlin@cds-snc.ca>
2024-05-14 13:04:46 -0700

    Fixing typos

--- templates/lambda_container/content/lambda.tf
+++ templates/lambda_container/content/lambda.tf
@@ -12,19 +12,18 @@ module "${{ values.product_name }}_lambda" {
   resource "aws_lambda_alias" "${{ values.product_name }}_lambda_alias" {
     name             = "latest"
     description      = "The latest version of the lambda function"
-    function_name    = module.${{ values.product_name }}.function_name
+    function_name    = module.${{ values.product_name }}_lambda.function_name
     function_version = "1"
   
     lifecycle {
       ignore_changes = [
         function_version,
-        routing_config
       ]
     }
   }
   
   resource "aws_lambda_function_url" "${{ values.product_name }}_lambda_url" {
-    function_name      = module.${{ values.product_name }}.function_name
+    function_name      = module.${{ values.product_name }}_lambda.function_name
     qualifier          = aws_lambda_alias.${{ values.product_name }}_lambda_alias.name
 
     authorization_type = "NONE"
