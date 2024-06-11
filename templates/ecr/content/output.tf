be8b4363860f891a1de4df2f1c47aa4aae477655
Sylvia McLaughlin <85905333+sylviamclaughlin@users.noreply.github.com>
2024-06-11 22:34:54 +0000

    Fixing little typos

--- templates/ecr/content/output.tf
+++ templates/ecr/content/output.tf
@@ -3,11 +3,11 @@
 ################################################################################
 
 output "ecr_repository_url" {
-  description = "URL of the URL shortener ECR"
-  value       = aws_ecr_repository.api.repository_url
+  description = "URL of the ${{ values.product_name }} ECR"
+  value       = aws_ecr_repository.${{ values.product_name }}_ecr.repository_url
 }
 
 output "ecr_repository_arn" {
-  description = "Arn of the ECR Repository"
-  value       = aws_ecr_repository.api.arn
+  description = "Arn of the ${{ values.product_name }} ECR Repository"
+  value       = aws_ecr_repository.${{ values.product_name }}_ecr.arn
 }
